59ea4ca4a972c04cd0af969a44ee79971bd04545
Shiz02 <78086780+Shiz02@users.noreply.github.com>
2021-03-24 10:47:50 +0700

    website

---
+++ js/code.js
@@ -0,0 +1,6 @@
+// let title = document.querySelector('ol li');
+// let arr = title.innerHTML.split(' ');
+// arr[0] = '<span>' + arr[0] + '</span>';
+// title.innerHTML = arr.join(' ');
+
+console.log('Hello, world!');
